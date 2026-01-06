1fe539a8fed66874acd7e9d069e03f4e035d8cff
Darlene-13 <darlenewendie@gmail.com>
2026-01-06 07:11:24 +0300

    Initialized todo springboot project

---
+++ Java_to_do/Java-to-do/src/test/java/darlene/github/io/javatodo/JavaToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package darlene.github.io.javatodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
